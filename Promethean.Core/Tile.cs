c34dd5acd5334f27663e50a69fce3d8addc41bd7
val <val.antonini@gmail.com>
2018-09-13 20:43:40 +1000

    - some constants

--- Promethean.Core/Tile.cs
+++ Promethean.Core/Tile.cs
@@ -2,8 +2,6 @@ namespace Promethean.Core
 {
     public static class Tile
     {
-        public static byte? Any => null;
-
         public static byte Open => 1;
 
         public static byte Floor => 0;
@@ -17,14 +15,17 @@ namespace Promethean.Core
         public static byte RightWall => 7;
         public static byte BottomWall => 8;
         public static byte LeftWall => 9;
-        // public static byte Floor => 0;
-        // public static byte Floor => 0;
+
+        public static byte TopLeftOutsideCorner => 10;
+        public static byte TopRightOutsideCorner => 11;
+        public static byte BottomLeftOutsideCorner => 12;
+        public static byte BottomRightOutsideCorner => 13;
 
     }
 
     public static class TileMask
     {
-        public static byte? wild => Tile.Any;
+        public static byte? wild => null;
         public static byte open => Tile.Open;
         public static byte blck => Tile.Floor;
     }
