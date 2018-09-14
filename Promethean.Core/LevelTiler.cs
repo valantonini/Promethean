467e442df934b219efb9aa693ea2b572e354d670
val <val.antonini@gmail.com>
2018-09-15 09:22:26 +1000

    - moving tile identification to outside drawn floor

--- Promethean.Core/LevelTiler.cs
+++ Promethean.Core/LevelTiler.cs
@@ -4,22 +4,33 @@ namespace Promethean.Core
 {
     public class LevelTiler
     {
+
         public void TileLevel(byte[,] level)
         {
+            var tilePoints = new List<TilePoint>();
             for (var row = 1; row < level.GetLength(0) - 1; row++)
             {
                 for (var column = 1; column < level.GetLength(1) - 1; column++)
                 {
-                    foreach (var tile in TileTypes.TileTypeList)
+                    foreach (var tile in TileMasks.TileMaskList)
                     {
                         if (GridEqualsMask(level, new Point(column, row), tile.Mask))
                         {
-                            level[row, column] = tile.Value;
+                            tilePoints.Add(new TilePoint()
+                            {
+                                TileType = tile.Value,
+                                Position = tile.Offset.Of(row, column)
+                            });
+                            //level[row, column] = tile.Value;
                             break;
                         }
                     }
 
                 }
+            }
+            foreach (var tilePoint in tilePoints)
+            {
+                level[tilePoint.Position.Row, tilePoint.Position.Column] = tilePoint.TileType;
             }
         }
 
@@ -58,6 +69,12 @@ namespace Promethean.Core
         }
     }
 
+    public class TilePoint
+    {
+        public Point Position { get; set; }
+        public byte TileType { get; set; }
+    }
+
     public class TileType
     {
         public string Name { get; set; }
@@ -65,14 +82,17 @@ namespace Promethean.Core
         public byte Value { get; set; }
 
         public byte?[,] Mask { get; set; }
+
+        public Point Offset { get; set; } = TilePositionOffsets.Current;
+
     }
 
-    public static class TileTypes
+    public static class TileMasks
     {
         private static byte? wild = TileMask.wild;
         private static byte open = TileMask.open;
         private static byte blck = TileMask.blck;
-        public static List<TileType> TileTypeList = new List<TileType>(){
+        public static List<TileType> TileMaskList = new List<TileType>(){
             new TileType
             {
                 Name = "Top Left Inside Corner",
@@ -81,7 +101,9 @@ namespace Promethean.Core
                     {wild,open,open},
                     {open,blck,blck},
                     {wild,blck,blck}
-                }
+                },
+                Offset = TilePositionOffsets.TopLeft
+
             },
 
             new TileType
@@ -92,7 +114,8 @@ namespace Promethean.Core
                     {open,open,wild},
                     {blck,blck,open},
                     {blck,blck,wild}
-                }
+                },
+                Offset = TilePositionOffsets.TopRight
             },
 
             new TileType
@@ -103,7 +126,8 @@ namespace Promethean.Core
                     {open,blck,blck},
                     {open,blck,blck},
                     {wild,open,wild}
-                }
+                },
+                Offset = TilePositionOffsets.BottomLeft
             },
 
             new TileType
@@ -114,8 +138,28 @@ namespace Promethean.Core
                     {blck,blck,open},
                     {blck,blck,open},
                     {wild,open,wild}
-                }
+                },
+                Offset = TilePositionOffsets.BottomRight
             }
         };
+    }
+
+    public static class TilePositionOffsets
+    {
+        public static Point TopLeft => new Point(-1, -1);
+        public static Point Top => new Point(0, -1);
+        public static Point TopRight => new Point(1, -1);
+
+        public static Point Left => new Point(-1, 0);
+        public static Point Current => new Point(0, 0);
+        public static Point Right => new Point(1, 0);
+        public static Point BottomLeft => new Point(-1, 1);
+        public static Point Bottom => new Point(0, 1);
+        public static Point BottomRight => new Point(1, 1);
+
+        public static Point Of(this Point point, int row, int column)
+        {
+            return new Point(column + point.Column, row + point.Row);
+        }
     }
 }
