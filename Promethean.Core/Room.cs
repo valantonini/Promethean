a6956e40f5f002c417286c094f6ef7980dbd6832
val <val.antonini@gmail.com>
2018-09-18 17:16:22 +1000

    - switched point to immutable struct to save dereferencing from arrays

--- Promethean.Core/Room.cs
+++ Promethean.Core/Room.cs
@@ -7,30 +7,14 @@ namespace Promethean.Core
         public int Height { get; set; }
         public Point Position { get; set; }
 
-        public Point RoomCentre => new Point()
-        {
-            X = Position.X + Height / 2,
-            Y = Position.Y + Width / 2
-        };
+        public Point RoomCentre => new Point(Position.X + Height / 2, Position.Y + Width / 2);
 
         public Point TopLeft => Position;
-        public Point BottomLeft => new Point()
-        {
-            X = Position.X + Height - 1,
-            Y = Position.Y
-        };
+        public Point BottomLeft => new Point(Position.X + Height - 1, Position.Y);
 
-        public Point TopRight => new Point()
-        {
-            X = Position.X,
-            Y = Position.Y + Width - 1
-        };
+        public Point TopRight => new Point(Position.X, Position.Y + Width - 1);
 
-        public Point BottomRight => new Point()
-        {
-            X = Position.X + Height - 1,
-            Y = Position.Y + Width - 1
-        };
+        public Point BottomRight => new Point(Position.X + Height - 1, Position.Y + Width - 1);
 
         public override string ToString()
         {
