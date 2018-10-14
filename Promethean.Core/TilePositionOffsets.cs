03f76cbf5cb91f2cda39be846950a48ca48af2c9
val <val.antonini@gmail.com>
2018-10-15 07:31:21 +1000

    - tile position offsets now readonly static instead of properties

--- Promethean.Core/TilePositionOffsets.cs
+++ Promethean.Core/TilePositionOffsets.cs
@@ -2,16 +2,16 @@ namespace Promethean.Core
 {
     public static class TilePositionOffsets
     {
-        public static Point TopLeft => new Point(-1, -1);
-        public static Point Top => new Point(-1, 0);
-        public static Point TopRight => new Point(-1, 1);
+        public static readonly Point TopLeft = new Point(-1, -1);
+        public static readonly Point Top = new Point(-1, 0);
+        public static readonly Point TopRight = new Point(-1, 1);
 
-        public static Point Left => new Point(0, -1);
-        public static Point Current => new Point(0, 0);
-        public static Point Right => new Point(0, 1);
-        public static Point BottomLeft => new Point(1, -1);
-        public static Point Bottom => new Point(1, 0);
-        public static Point BottomRight => new Point(1, 1);
+        public static readonly Point Left = new Point(0, -1);
+        public static readonly Point Current = new Point(0, 0);
+        public static readonly Point Right = new Point(0, 1);
+        public static readonly Point BottomLeft = new Point(1, -1);
+        public static readonly Point Bottom = new Point(1, 0);
+        public static readonly Point BottomRight = new Point(1, 1);
 
         public static Point Of(this Point point, int row, int column)
         {
