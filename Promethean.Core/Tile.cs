ff0e3b620745591a7d130c0ef39191d43a9ca081
val <val.antonini@gmail.com>
2018-10-15 07:20:09 +1000

    - fixed tile class to be const intead of properties

--- Promethean.Core/Tile.cs
+++ Promethean.Core/Tile.cs
@@ -2,32 +2,24 @@ namespace Promethean.Core
 {
     public static class Tile
     {
-        public static byte Empty => 1;
+        public const byte Empty = 1;
 
-        public static byte Floor => 0;
+        public const byte Floor = 0;
 
-        public static byte TopLeftInsideCorner => 2;
-        public static byte TopRightInsideCorner => 3;
-        public static byte BottomLeftInsideCorner => 4;
-        public static byte BottomRightInsideCorner => 5;
+        public const byte TopLeftInsideCorner = 2;
+        public const byte TopRightInsideCorner = 3;
+        public const byte BottomLeftInsideCorner = 4;
+        public const byte BottomRightInsideCorner = 5;
 
-        public static byte TopWall => 6;
-        public static byte RightWall => 7;
-        public static byte BottomWall => 8;
-        public static byte LeftWall => 9;
+        public const byte TopWall = 6;
+        public const byte RightWall = 7;
+        public const byte BottomWall = 8;
+        public const byte LeftWall = 9;
 
-        public static byte TopLeftOutsideCorner => 10;
-        public static byte TopRightOutsideCorner => 11;
-        public static byte BottomLeftOutsideCorner => 12;
-        public static byte BottomRightOutsideCorner => 13;
-
-        public static byte TopNub => 14;
-        public static byte RightNub => 15;
-        public static byte BottomNub => 16;
-        public static byte LeftNub => 17;
-
-        public static byte DoubleVerticalWall => 18;
-        public static byte DoubleHorizontalWall => 19;
+        public const byte TopLeftOutsideCorner = 10;
+        public const byte TopRightOutsideCorner = 11;
+        public const byte BottomLeftOutsideCorner = 12;
+        public const byte BottomRightOutsideCorner = 13;
     }
 
     public static class TileMask
